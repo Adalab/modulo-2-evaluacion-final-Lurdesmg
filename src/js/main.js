47fc1c517df1aaaed0e1479642327c93d8ea0b34
Lurdesmg <lurdesxi07@gmail.com>
2023-02-25 23:20:26 +0100

    add function for empty image

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 'use strict';
 
@@ -10,43 +11,53 @@ const favElement = document.querySelector('.js-favourites');
 
 // Variables creadas en JS
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const emptyImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=No-Image';
 
 let listCocktailsData = [];
 
-
-// Funciones
+// Función de evento click en el btn buscar, donde nos trae los datos del servidor
 function handleSubmit(ev) {
     ev.preventDefault();
     const inputValue = inputSearch.value;
-    console.log(inputValue);
     fetch(`${url}${inputValue}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data.drinks);
       listCocktailsData = data.drinks.map((cocktail) =>({
         id: cocktail.idDrink,
         name: cocktail.strDrink,
-        image: cocktail.strDrinkThumb,
+        image: checkImage(cocktail.strDrinkThumb),
       }));
+      // Llamada de la funcion para que pinte los datos al html
         renderListCocktails(listCocktailsData);
     });
 }
 
+// Funcion para que cunmpla la condición de la imagen si viene undefined/null/vacia
+function checkImage(strDrinkThumb) {
+    if (strDrinkThumb) {
+        return strDrinkThumb;
+    } else {
+        return emptyImage;
+    }
+}
+
+// Función que pinta la lista de los cockteles
 function renderListCocktails(listCocktailsData) {
-    console.log(listCocktailsData);
-        for (const cocktail of listCocktailsData) {
-            listElement.innerHTML += renderCocktail(cocktail);
-        }
+    for (const cocktail of listCocktailsData) {
+        listElement.innerHTML += renderCocktail(cocktail);
     }
-    
-    function renderCocktail(cocktail) {
-        const html = `<li>
-        <img src="${cocktail.image}" alt="${cocktail.name}">
-        <h3>${cocktail.name}</h3>
-        </li>`;
-        return html;
-    }
+}
 
 
-// Eventos
+// Función donde estamos pintado la estructura del html para cada cocktail
+function renderCocktail(cocktail) {
+    const html = `<li>
+    <img src="${cocktail.image}" alt="${cocktail.name}">
+    <h3>${cocktail.name}</h3>
+    </li>`;
+    return html;
+}
+
+
+// Evento btn buscar
 btnSubmit.addEventListener('click' , handleSubmit);
