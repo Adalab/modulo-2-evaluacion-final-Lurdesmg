ed416b892df7533824aeb40d2a87a699250e9b10
Lurdesmg <lurdesxi07@gmail.com>
2023-02-25 22:20:58 +0100

    add event and fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,38 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-console.log('>> Ready :)');
+// Elementos del Html
+const inputSearch = document.querySelector('.js-search');
+const btnSubmit = document.querySelector('.js-submit');
+const btnReset = document.querySelector('.js-reset');
+const listElement = document.querySelector('.js-list');
+const favElement = document.querySelector('.js-favourites');
+
+// Variables creadas en JS
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+let listCocktailsData = [];
+
+
+// Funciones
+function handleSubmit(ev) {
+    ev.preventDefault();
+    const inputValue = inputSearch.value;
+    console.log(inputValue);
+    fetch(`${url}${inputValue}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.drinks);
+      listCocktailsData = data.drinks.map((cocktail) =>({
+        id: cocktail.idDrink,
+        name: cocktail.strDrink,
+        image: cocktail.strDrinkThumb,
+      }));
+     console.log(listCocktailsData)
+    });
+}
+
+
+
+// Eventos
+btnSubmit.addEventListener('click' , handleSubmit);
